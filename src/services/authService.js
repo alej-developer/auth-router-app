c60fd22b762b6484d775696823628d0d00af1058
alej-developer <alejandropm32@gmail.com>
2026-03-01 13:45:18 +0100

    feat: creación de servicio de validación de credenciales

---
+++ src/services/authService.js
@@ -0,0 +1,5 @@
+export const CheckCredentials = (email, password) => {
+    const validEmail = "[EMAIL_ADDRESS]";
+    const validPassword = "[PASSWORD]";
+    return email === validEmail && password === validPassword;
+};
